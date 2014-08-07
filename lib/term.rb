def18e36f9375d09047468aa48b6969aac8b35f7
Diego Avalon & Kyle Smith <student@epicodus.com>
2014-08-07 09:52:16 -0700

    initial commit

---
+++ lib/term.rb
@@ -0,0 +1,10 @@
+class Term
+
+attr_reader(:word, :definition)
+
+  def initialize(attributes)
+  @word = attributes[:word]
+  @definition = attributes[:definition]
+  end
+
+end
---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,9 @@
+require 'term'
+require 'rspec'
+
+describe Term do
+  it 'initializes an instance of Term' do
+    test_term = Term.new({:word => "carrot", :definition => 'a orange vegetable'})
+    expect(test_term).to be_an_instance_of Term
+  end
+end
